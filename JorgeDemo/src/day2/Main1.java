39efca3f32113447d362fa2ab17a1b9857abf4a3
ParaHuang <paradox041@gmail.com>
2025-01-09 10:03:44 +0800

    day2

---
+++ JorgeDemo/src/day2/Main1.java
@@ -0,0 +1,65 @@
+package day2;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 1.if...
+		 if(statement){
+		 	operation
+		 }
+		 2.if...else...
+		 if(statement){
+		 	operationA
+		 }else{
+		 	operationsB
+		 }
+		 
+		 3. if...else if...else if...else...
+		 if(statement1){
+		 	operationA
+		 }else if(statement2){
+		 	operationB
+		 }else if(statement3){
+		 	operationC
+		 }else{
+		 	operationD
+		 }
+		 
+		 
+		 
+		 
+		 */
+		
+		/*
+		int num = 12;
+		if(num%2 == 0) {
+			System.out.println(num+" is an even number");
+		}else {
+			System.out.println(num+" is an odd number");
+		}
+		*/
+		
+		int grade = 87;   //0~100
+		//1~100
+		//<60   not pass
+		//60~69 pass
+		//70~79 good
+		//80~100 excellent
+		if(grade<60) {
+			System.out.println("not pass\n" + grade);
+		}else if(grade < 70) {
+			System.out.println("pass\n" + grade);
+		}else if(grade < 80) {
+			System.out.println("good\n" + grade);
+		}else {
+			System.out.println("excellent\n" + grade);
+		}
+		
+		
+		
+		
+	}
+
+}
---
+++ JorgeDemo/src/day2/Main2.java
@@ -0,0 +1,18 @@
+package day2;
+
+import java.util.Scanner;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//show options:   alt + /
+		Scanner scanner = new Scanner(System.in);
+		
+		int num = scanner.nextInt();
+		
+		
+		System.out.println("you just entered:"+num);
+	}
+
+}
